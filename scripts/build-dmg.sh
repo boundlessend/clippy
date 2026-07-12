f417f8853c1816e26c2e63236552ba3ca4726ffa
boundlessend <192182985+boundlessend@users.noreply.github.com>
2026-07-12 19:47:10 +0300

    chore: lowercase the boundlessend nick in license and about

--- scripts/build-dmg.sh
+++ scripts/build-dmg.sh
@@ -42,7 +42,7 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <dict>
     <key>NSAllowsLocalNetworking</key><true/>
   </dict>
-  <key>NSHumanReadableCopyright</key><string>© 2026 Boundlessend</string>
+  <key>NSHumanReadableCopyright</key><string>© 2026 boundlessend</string>
   <key>CFBundleDocumentTypes</key>
   <array>
     <dict>
